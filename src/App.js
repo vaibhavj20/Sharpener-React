bc25c7c7fc1f05c1230fe9655ac626a7280a0973
Vaibhav <vaibhavjam86@gmail.com>
2024-04-08 09:32:25 +0530

    React Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h1>I'am Learning React</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
