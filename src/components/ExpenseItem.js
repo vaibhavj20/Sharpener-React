9f52da12f142ed148274d96f08e12b28948832a1
Vaibhav <vaibhavjam86@gmail.com>
2024-04-08 10:04:21 +0530

    Writing Complex JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 function ExpenseItem() {
   return (
     <div>
-      <p>Food - $5</p>
-      <p>Petrol - $20</p>
-      <p>Movie - $10</p>
+      <div>15 August 2023</div>
+      <div>Book</div>
+      <div>$10</div>
+      <div>Delhi</div>
     </div>
   );
 }
