8670b658fa2337747c0d56aaa5bb6bb7ee478a51
Vaibhav <vaibhavjam86@gmail.com>
2024-04-09 15:55:00 +0530

    Formatting Date - Giving Calendar Look

--- src/App.js
+++ src/App.js
@@ -2,47 +2,22 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    {
-      id: "1",
-      date: new Date(2023, 7, 15),
-      title: "Insurance",
-      price: 100,
-      location: "Bangalore",
-    },
-    {
-      id: "2",
-      date: new Date(2023, 3, 25),
-      title: "Book",
-      price: 10,
-      location: "Delhi",
-    },
-    {
-      id: "3",
-      date: new Date(2023, 10, 11),
-      title: "Pen",
-      price: 1,
-      location: "Hyderabad",
-    },
-    {
-      id: "4",
-      date: new Date(2023, 1, 14),
-      title: "Laptop",
-      price: 200,
-      location: "Mumbai",
-    },
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
   ];
 
   return (
     <div>
-      <h1>Let's get Started</h1>
-      {expenses.map((expense, index) => {
+      <h2>Let's get Started</h2>
+      {expenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
-            date={expense.date}
             title={expense.title}
+            date={expense.date}
             price={expense.price}
-            location={expense.location}
           ></ExpenseItem>
         );
       })}
