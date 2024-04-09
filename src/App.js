8a6d1e19f8e602c796a952b4a2d45b576f6c05b3
Vaibhav <vaibhavjam86@gmail.com>
2024-04-09 15:44:15 +0530

    Props Object & Passing Data to Child Component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      <h1>I'am Learning React</h1>
-      <ExpenseItem></ExpenseItem>
+      <h1>Let's get Started</h1>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        title="Insurance"
+        price="50"
+        location="Bangalore"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+        location="Delhi"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+        location="Hyderabad"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        title="Laptop"
+        price="200"
+        location="Mumbai"
+      ></ExpenseItem>
     </div>
   );
 }
