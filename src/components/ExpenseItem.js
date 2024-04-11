3d69b419dbfcd8fbc9e9351beed9960465b2b89d
Vaibhav <vaibhavjam86@gmail.com>
2024-04-11 09:17:48 +0530

    Splitting and wrapping the components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,30 +1,10 @@
 import "./ExpenseItem.css";
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-function ExpenseItem(props) {
-  const month = months[props.date.getMonth()];
-  const year = props.date.getFullYear();
-  const date = props.date.getDate();
+import ExpenseDate from "./ExpenseDate";
 
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{date}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
