403dbca51248f31928dd6a5c3d254d8175fe050a
Vaibhav <vaibhavjam86@gmail.com>
2024-04-26 20:04:55 +0530

    Expense Chart

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,13 @@
+// Write your code below:
+import React from "react";
+import Chart from "../Chart/Chart";
+
+const ExpenseChart = (props) => {
+  return (
+    <div>
+      <Chart chartExpenses={props.chartData} />
+    </div>
+  );
+};
+
+export default ExpenseChart;
