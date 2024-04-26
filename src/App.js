c2755d389bdad8fd08f12fcd41b2ae1024aba836
Vaibhav <vaibhavjam86@gmail.com>
2024-04-26 20:05:22 +0530

    Updated File

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-// import ExpenseItem from "./components/Expenses/ExpenseItem";
 import React, { useState } from "react";
-// import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+  { id: 2, title: "Book", date: new Date(2024, 8, 25), price: 10 },
+  { id: 3, title: "Pen", date: new Date(2025, 2, 10), price: 1 },
+  { id: 4, title: "Laptop", date: new Date(2024, 9, 17), price: 200 },
+];
+
 const App = () => {
-  const [expenses, setExpenses] = useState([
-    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
-    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
-    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
-    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
-  ]);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    // Update expenses state to include the new expense
-    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
