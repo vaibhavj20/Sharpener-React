d19c8919d609bc7bfc3c8cebb7673d590d81f271
Vaibhav <vaibhavjam86@gmail.com>
2024-04-26 18:50:07 +0530

    Multiple to One State

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,17 +1,52 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+const userEnteredDetails={
+  enteredTitle="",
+  enteredAmount="",
+  enteredDate="",
+  
+};
 
+const ExpenseForm = () => {
+ const [userInput,setUserInput]=useState(userEnteredDetails);
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+   // setUserInput({
+   //   ...userInput,
+   //   enteredTitle=event.target.value,
+   // });
+    setUserInput((prevState)=>{
+      return{
+        ...userInput,
+        enteredTitle=event.target.value,
+       }
+    });
   };
+
   const amountChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+   //  setUserInput({
+   //   ...userInput,
+   //   enteredAmount=event.target.value,
+   // });
+      setUserInput((prevState)=>{
+      return{
+        ...userInput,
+        enteredAmount = event.target.value,
+       }
+    });
   };
+
   const dateChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+   // setUserInput({
+   //   ...userInput,
+   //   enteredDate=event.target.value,
+   // });
+      setUserInput((prevState)=>{
+      return{
+        ...userInput,
+        enteredDate = event.target.value,
+       }
+    });
   };
 
   return (
