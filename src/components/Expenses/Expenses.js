24f0d279e41baa4528934a449c41320d1233f81d
Vaibhav <vaibhavjam86@gmail.com>
2024-04-26 19:51:21 +0530

    Updated Fike

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
@@ -15,33 +15,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-  let expenseContent1 = "";
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-      return (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          price={expense.price}
-        />
-      );
-    });
-    if (filteredExpenses.length === 1) {
-      expenseContent1 = <p>Only one expense found. Please add more</p>;
-    }
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
-      {expensesContent}
-      {expenseContent1}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
